0b2727904858d20d660e6f3c0e61ae76bdf860f6
dongjilo <121302078+dongjilo@users.noreply.github.com>
2023-09-16 20:16:15 +0800

    bug fix

--- calculator.php
+++ calculator.php
@@ -1,5 +1,6 @@
 <?php
-
+session_start();
+$_SESSION['errorMsg'] = "<div class='alert alert-danger fade show' role='alert'> Please fill out all the fields. </div>";
 function addNum($x, $y){
     return $x + $y;
 }
@@ -17,7 +18,7 @@ function divNum($x, $y){
 }
 
 function checkRange($x){
-    if ($x > 0 && $x < 10) {
+    if ($x >= 0 && $x <= 10) {
         return true;
     } else {
         return false;
@@ -35,12 +36,14 @@ function checkRange($x){
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="./css/bootstrap.min.css">
     <script src="./js/bootstrap.bundle.min.js"></script>
+    <script src="./js/ajax.js"></script>
     <title>Calculator</title>
 </head>
 <body style="background-color: #eee">
     <div class="container border p-5 bg-light" style="margin-top: 9%; max-width: 500px">
-        <h1>Calculator</h1>
-        <form action="<?php $_PHP_SELF ?>" method="post">
+        <h1 class="h1 text-center pb-3">Calculator</h1>
+        <p class="small">Enter numbers from 1-10</p>
+        <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
             <div class="form-floating mb-3">
                 <input type="number" class="form-control" id="floatingNum1" name="num1" placeholder="1">
                 <label for="floatingInput">Enter 1st number</label>
@@ -66,25 +69,57 @@ function checkRange($x){
         <?php
         if (isset($_POST['selectAction'])){
             $selectedAction = $_POST['selectAction'];
-            if (isset($_POST['num1']) && isset($_POST['num2']))
+            if (isset($_POST['num1']) && isset($_POST['num2'])) {
                 $x = $_POST['num1'];
                 $y = $_POST['num2'];
-                switch ($selectedAction) {
-                    case "addNum":
-                        echo "Sum is: " . addNum($x, $y);
-                        break;
-                    case "subNum":
-                        echo "Difference is: " . subNum($x, $y);
-                        break;
-                    case "mulNum":
-                        echo "Product is: " . mulNum($x, $y);
-                        break;
-                    case "divNum":
-                        echo "Quotient is: " . divNum($x, $y);
-                        break;
-                    default:
-                        echo "Please fill out all the fields.";
+                $status = (checkRange($x)) ? "in range" : "not in range";
+                $status = (checkRange($y)) ? "in range" : "not in range";
+
+                if (!empty($x) && !empty($y)) {
+                    switch ($selectedAction) {
+                        case "addNum":
+                            echo "<div class='alert alert-success fade show' role='alert'> Sum of $x and $y is " . addNum($x, $y);
+                            if ($x > 0 && $x < 10) {
+                                echo "<br>$x is $status and $y is $status.</div>";
+                            } else {
+                                echo "<br>$x is $status and $y is $status.</div>";
+                            }
+                            unset($_SESSION['errorMsg']);
+                            break;
+                        case "subNum":
+                            echo "<div class='alert alert-success fade show' role='alert'> Difference of $x and $y is " . subNum($x, $y);
+                            if ($x > 0 && $x < 10) {
+                                echo "<br>$x is $status and $y is $status.</div>";
+                            } else {
+                                echo "<br>$x is $status and $y is $status.</div>";
+                            }
+                            unset($_SESSION['errorMsg']);
+                            break;
+                        case "mulNum":
+                            echo "<div class='alert alert-success fade show' role='alert'> Product of $x and $y is " . mulNum($x, $y);
+                            if ($x > 0 && $x < 10) {
+                                echo "<br>$x is $status and $y is $status.</div>";
+                            } else {
+                                echo "<br>$x is $status and $y is $status.</div>";
+                            }
+                            unset($_SESSION['errorMsg']);
+                            break;
+                        case "divNum":
+                            echo "<div class='alert alert-success fade show' role='alert'> Quotient of $x and $y is " . divNum($x, $y);
+                            if ($x > 0 && $x < 10) {
+                                echo "<br>$x is $status and $y is $status.</div>";
+                            } else {
+                                echo "<br>$x is $status and $y is $status.</div>";
+                            }
+                            unset($_SESSION['errorMsg']);
+                            break;
+                        default:
+                            echo $_SESSION['errorMsg'];
+                    }
+                } else {
+                    echo $_SESSION['errorMsg'];
                 }
+            }
         }
         ?>
     </div>
